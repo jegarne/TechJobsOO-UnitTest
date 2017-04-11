950ab1fa365d27b5b50e38d3980a539a3d67f1f2
Jeremy Garner <jegarne@gmail.com>
2017-04-10 19:49:34 -0500

    initial commit

---
+++ src/TechJobs/ViewModels/JobFieldsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using TechJobs.Models;
+
+namespace TechJobs.ViewModels
+{
+    public class JobFieldsViewModel : BaseViewModel
+    {
+        // TODO #7.2 - Extract members common to SearchJobsViewModel
+        // to BaseViewModel
+
+        // The current column
+        public JobFieldType Column { get; set; }
+
+        // All fields in the given column
+        public IEnumerable<JobField> Fields { get; set; }
+        
+    }
+}
---
+++ src/TechJobs/ViewModels/SearchJobsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using TechJobs.Models;
+
+namespace TechJobs.ViewModels
+{
+    public class SearchJobsViewModel : BaseViewModel
+    {
+        // TODO #7.1 - Extract members common to JobFieldsViewModel
+        // to BaseViewModel
+
+        // The search results
+        public List<Job> Jobs { get; set; }
+
+        // The column to search, defaults to all
+        public JobFieldType Column { get; set; } = JobFieldType.All;
+
+        // The search value
+        [Display(Name = "Keyword:")]
+        public string Value { get; set; } = "";
+        
+    }
+}
